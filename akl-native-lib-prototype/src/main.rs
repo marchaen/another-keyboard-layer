c81e7a27d4eba71c69c6acc4c52a58b0879ff8c0
marchaen <marchaen.dev@gmail.com>
2023-10-27 07:55:06 +0200

    Remove translated character hooks
    Windows doesn't let you manipulate WM_CHAR messages for other windows
    so everything related to translated characters is completely useless for
    the application of akl.

--- akl-native-lib-prototype/src/main.rs
+++ akl-native-lib-prototype/src/main.rs
@@ -3,20 +3,13 @@ mod translation;
 
 use std::ptr;
 
-use windows::{
-    core::{PCSTR, PCWSTR},
-    Win32::{
-        Foundation::{GetLastError, BOOL, HMODULE, LPARAM, LRESULT, WPARAM},
-        System::{
-            Console::{SetConsoleCtrlHandler, CTRL_BREAK_EVENT, CTRL_CLOSE_EVENT, CTRL_C_EVENT},
-            LibraryLoader::{FreeLibrary, GetProcAddress, LoadLibraryW},
-        },
-        UI::WindowsAndMessaging::{
-            CallNextHookEx, DispatchMessageW, GetMessageW, SetWindowsHookExW, TranslateMessage,
-            UnhookWindowsHookEx, HHOOK, HOOKPROC, KBDLLHOOKSTRUCT, MSG, WH_GETMESSAGE,
-            WH_KEYBOARD_LL, WINDOWS_HOOK_ID, WM_KEYDOWN, WM_KEYFIRST, WM_KEYLAST, WM_KEYUP,
-            WM_SYSKEYDOWN, WM_SYSKEYUP,
-        },
+use windows::Win32::{
+    Foundation::{GetLastError, BOOL, HMODULE, LPARAM, LRESULT, WPARAM},
+    System::Console::{SetConsoleCtrlHandler, CTRL_BREAK_EVENT, CTRL_CLOSE_EVENT, CTRL_C_EVENT},
+    UI::WindowsAndMessaging::{
+        CallNextHookEx, DispatchMessageW, GetMessageW, SetWindowsHookExW, TranslateMessage,
+        UnhookWindowsHookEx, HHOOK, HOOKPROC, KBDLLHOOKSTRUCT, MSG, WH_KEYBOARD_LL,
+        WINDOWS_HOOK_ID, WM_KEYDOWN, WM_KEYFIRST, WM_KEYLAST, WM_KEYUP, WM_SYSKEYDOWN, WM_SYSKEYUP,
     },
 };
 
@@ -30,19 +23,6 @@ fn main() {
         "global raw keyboard".to_owned(),
         WH_KEYBOARD_LL,
         Some(raw_keyboard_input_hook),
-        None,
-    );
-
-    let character_listener = ListenerHandle::load(
-        "akl_translated_character_hook",
-        "raw_translated_character_hook",
-    );
-
-    let character_hook = HookHandle::register(
-        "translated character".to_owned(),
-        WH_GETMESSAGE,
-        character_listener.listener,
-        character_listener.dll,
     );
 
     // Explicit shutdown callback is needed because windows will kill the
@@ -54,8 +34,6 @@ fn main() {
     // needed in the real library implementation because the c# clients will
     // take care of graceful shutdown instead.
     set_shutdown_callback(move || {
-        drop(character_hook);
-        drop(character_listener);
         drop(keyboard_hook);
         Debugger::destroy();
     });
@@ -89,122 +67,19 @@ unsafe extern "system" fn raw_shutdown_handler(ctrltype: u32) -> BOOL {
     true.into()
 }
 
-// TODO: Research if there is a way to guarantee that the listener won't be
-// called after the handle is dropped. (Maybe something with lifetimes and
-// phantom data could be used here.)
-struct ListenerHandle {
-    dll_name: String,
-    listener_name: String,
-    dll: Option<HMODULE>,
-    listener: HOOKPROC,
-}
-
-impl ListenerHandle {
-    fn load<S: Into<String>>(dll_name: S, listener_name: S) -> Self {
-        let dll_name = dll_name.into();
-        let listener_name = listener_name.into();
-
-        Debugger::write(&format!(
-            "Load dll {dll_name} for listener hook {listener_name}"
-        ));
-
-        assert!(
-            listener_name.is_ascii(),
-            "The name of the listener \"{listener_name}\" must be ascii only."
-        );
-
-        let dll_name_utf16 = dll_name
-            .encode_utf16()
-            .chain(std::iter::once(0))
-            .collect::<Vec<u16>>();
-
-        // See https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-loadlibraryw
-        // and https://learn.microsoft.com/en-us/windows/win32/winprog/windows-data-types
-        // for information about invariants. The type which is needed here
-        // (pcwstr) is called LPCWSTR in the windows documentation.
-        let load_result = unsafe { LoadLibraryW(PCWSTR::from_raw(dll_name_utf16.as_ptr())) };
-
-        match load_result {
-            Ok(handle) => {
-                Debugger::write(&format!("Successfully loaded dll {dll_name}",));
-
-                // See https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-getprocaddress
-                // for details. The type which is needed here is called lpcstr
-                // and has to be ascii only.
-                let maybe_listener =
-                    unsafe { GetProcAddress(handle, PCSTR::from_raw(listener_name.as_ptr())) };
-
-                // Safety: We control both this program and the target dll which
-                // means the specified method will never not be a hook function.
-                let listener: HOOKPROC = unsafe { std::mem::transmute(maybe_listener) };
-
-                assert!(
-                    listener.is_some(),
-                    "Couldn't locate function {listener_name} in dll {dll_name}"
-                );
-
-                Self {
-                    dll_name,
-                    listener_name,
-                    dll: Some(handle),
-                    listener,
-                }
-            }
-            Err(error) => panic!(
-                "Trying to load dll {dll_name} failed: {} ({})",
-                error.message().to_string_lossy(),
-                error.code()
-            ),
-        }
-    }
-
-    fn unload(&mut self) {
-        if self.dll.is_none() {
-            return;
-        }
-
-        self.listener = None;
-        let dll = self.dll.take().unwrap();
-
-        Debugger::write(&format!(
-            "Unload dll {} and listener hook {}",
-            self.dll_name, self.listener_name
-        ));
-
-        // See https://learn.microsoft.com/en-us/windows/win32/api/libloaderapi/nf-libloaderapi-freelibrary
-        let result = unsafe { FreeLibrary(dll) };
-
-        Debugger::write(&format!(
-            "Unload dll {} and listener hook {} result: {result:?}",
-            self.dll_name, self.listener_name
-        ));
-    }
-}
-
-impl Drop for ListenerHandle {
-    fn drop(&mut self) {
-        self.unload();
-    }
-}
-
 struct HookHandle {
     hook: HHOOK,
     name: String,
 }
 
 impl HookHandle {
-    fn register(
-        name: String,
-        id: WINDOWS_HOOK_ID,
-        listener: HOOKPROC,
-        dll: Option<HMODULE>,
-    ) -> Self {
+    fn register(name: String, id: WINDOWS_HOOK_ID, listener: HOOKPROC) -> Self {
         Debugger::write(&format!("Register {name} listener hook."));
 
         let register_result = unsafe {
             // Details and safety see:
             // https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setwindowshookexw
-            SetWindowsHookExW(id, listener, dll.unwrap_or(HMODULE(0)), 0)
+            SetWindowsHookExW(id, listener, HMODULE(0), 0)
         };
 
         match register_result {
