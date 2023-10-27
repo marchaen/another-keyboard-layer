050925a198c27e013908f1e766d79675fed754dc
marchaen <marchaen.dev@gmail.com>
2023-10-27 07:55:07 +0200

    Add more details to the documentation and clarify

--- build-documentation.py
+++ build-documentation.py
@@ -4,16 +4,19 @@ import shutil
 import subprocess
 from pathlib import Path
 
-def generate_main_documentation(out_dir: Path):
+def generate_general_documentation(out_dir: Path):
     asciidoctor = shutil.which("asciidoctor")
+    asciidoctor_pdf = shutil.which("asciidoctor-pdf")
 
-    if asciidoctor is None:
+    if asciidoctor is None or asciidoctor_pdf is None:
         print("""
-    Please install asciidoctor with the asciidoctor-diagram extension:
+    Please install asciidoctor with the asciidoctor-diagram extension and
+    asciidoctor-pdf:
 
     https://docs.asciidoctor.org/asciidoctor/latest/install/ruby-packaging/
+    https://docs.asciidoctor.org/pdf-converter/latest/install/#install-asciidoctor-pdf
     https://docs.asciidoctor.org/diagram-extension/latest/installation/
-    https://docs.asciidoctor.org/asciidoctor/latest/syntax-highlighting/rouge/'
+    https://docs.asciidoctor.org/asciidoctor/latest/syntax-highlighting/rouge/
               """)
         exit()
 
@@ -21,17 +24,25 @@ def generate_main_documentation(out_dir: Path):
         "git rev-parse --short HEAD", shell=True, capture_output=True
     ).stdout.decode()
 
+    # Html output
     subprocess.run([
-        asciidoctor, "-r", "asciidoctor-diagram", "-acommit-hash=" + git_commit, 
+        asciidoctor, "-r", "asciidoctor-diagram", "-a", "commit-hash=" + git_commit, 
         "--destination-dir", out_dir, "README.adoc"
     ])
 
+    # Pdf output
     subprocess.run([
-        asciidoctor, "-b", "manpage", "-acommit-hash=" + git_commit, 
+        asciidoctor_pdf, "-r", "asciidoctor-diagram", "-a", "commit-hash=" + git_commit,
+        "--destination-dir", out_dir, "README.adoc"
+    ])
+
+    # Manpage for the cli
+    subprocess.run([
+        asciidoctor, "-b", "manpage", "-a", "commit-hash=" + git_commit, 
         "--destination-dir", out_dir.joinpath("man", "man1"), "Manpage.adoc"
     ])
 
-def generate_library_documentation():
+def generate_core_system_lib_documentation():
     subprocess.run(
         "cargo doc --no-deps --document-private-items --target x86_64-pc-windows-gnu", 
         shell=True, stderr=subprocess.DEVNULL, cwd="akl-core-system-lib"
@@ -49,5 +60,5 @@ if out_dir.exists():
 
 out_dir.mkdir()
 
-generate_main_documentation(out_dir)
-generate_library_documentation()
+generate_general_documentation(out_dir)
+generate_core_system_lib_documentation()
