d9b202db4fb4c9cee97ffdacf2d383e830c5ae66
marchaen <marchaen.dev@gmail.com>
2023-10-27 07:55:06 +0200

    Improve build automation and related documentation

--- akl-core-system-lib/build.rs
+++ akl-core-system-lib/build.rs
@@ -1,7 +1,7 @@
 fn main() {
     csbindgen::Builder::default()
         .input_extern_file("src/lib.rs")
-        .csharp_dll_name("libakl_core_system_lib")
+        .csharp_dll_name("akl_core_system_lib")
         .csharp_namespace("AKL.Core")
         .csharp_class_name("AklCoreNativeInterface")
         .generate_csharp_file("target/bindings/AklCoreNativeInterface.g.cs")
