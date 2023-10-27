aff81416f35a2d764c5ad2649aa20e6b762245f1
marchaen <marchaen.dev@gmail.com>
2023-10-27 07:55:06 +0200

    Add prototype target dir to clean up script

--- clean-output.sh
+++ clean-output.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 rm -rf ./akl-core-system-lib/target
+rm -rf ./akl-native-lib-prototype/target
 
 # Loop over all directories that start with the prefix "AKL." and delete their
 # "bin" and "obj" folders.
