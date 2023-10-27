a2079ab26ef8cc2aa54ba5f3c10a99994b79fcc5
marchaen <marchaen.dev@gmail.com>
2023-10-27 07:55:06 +0200

    Move event processor to core lib

--- akl-core-system-lib/src/lib.rs
+++ akl-core-system-lib/src/lib.rs
@@ -4,9 +4,11 @@ mod ffi;
 mod handle;
 mod key;
 
-use handle::AklHandle;
+use std::collections;
 
 use thiserror::Error;
+
+use handle::AklHandle;
 use key::{Key, KeyCombination};
 
 #[derive(Error, Debug)]
@@ -23,7 +25,7 @@ pub enum AklError {
 pub struct Configuration {
     pub switch_key: Option<Key>,
     pub default_combination: Option<KeyCombination>,
-    pub mappings: std::collections::HashMap<KeyCombination, KeyCombination>,
+    pub mappings: collections::HashMap<KeyCombination, KeyCombination>,
 }
 
 #[derive(Default)]
@@ -79,3 +81,112 @@ impl Drop for AnotherKeyboardLayer {
         }
     }
 }
+
+#[derive(Debug, Clone, Copy)]
+enum Action {
+    Press,
+    Release,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Event {
+    pub action: Action,
+    pub key: Key,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum ChangeEventRequest {
+    None,
+    Block,
+    ReplaceWith(KeyCombination),
+}
+
+struct EventProcessor {
+    switch_key: Key,
+    default_combination: Option<KeyCombination>,
+    mappings: collections::HashMap<KeyCombination, KeyCombination>,
+    currently_pressed: Vec<Key>,
+    block_events: bool,
+    key_combination_executed: bool,
+}
+
+impl From<Configuration> for EventProcessor {
+    fn from(value: Configuration) -> Self {
+        Self {
+            switch_key: value
+                .switch_key
+                .expect("Switch key should be valid for an event processor."),
+            default_combination: value.default_combination,
+            mappings: value.mappings,
+            currently_pressed: vec![],
+            block_events: false,
+            key_combination_executed: false,
+        }
+    }
+}
+
+impl EventProcessor {
+    pub fn process(&mut self, event: Event) -> ChangeEventRequest {
+        match event.action {
+            Action::Press => {
+                if event.key == self.switch_key {
+                    self.block_events = true;
+                    self.currently_pressed.clear();
+                    return ChangeEventRequest::Block;
+                }
+
+                if !self.block_events {
+                    return ChangeEventRequest::None;
+                }
+
+                self.currently_pressed.push(event.key);
+
+                let maybe_target_combination: Result<KeyCombination, _> =
+                    self.currently_pressed.as_slice().try_into();
+
+                if let Ok(target_combination) = maybe_target_combination {
+                    if let Some(replacement_combination) =
+                        self.mappings.get(&target_combination)
+                    {
+                        self.key_combination_executed = true;
+                        self.currently_pressed.pop();
+                        return ChangeEventRequest::ReplaceWith(
+                            *replacement_combination,
+                        );
+                    }
+                }
+
+                ChangeEventRequest::Block
+            }
+            Action::Release => {
+                if event.key == self.switch_key {
+                    self.block_events = false;
+
+                    if !self.key_combination_executed {
+                        if let Some(combination) = self.default_combination {
+                            return ChangeEventRequest::ReplaceWith(
+                                combination,
+                            );
+                        }
+                    }
+
+                    self.key_combination_executed = false;
+                    return ChangeEventRequest::Block;
+                }
+
+                if let Ok(index) =
+                    self.currently_pressed.binary_search(&event.key)
+                {
+                    self.currently_pressed.swap_remove(index);
+
+                    if self.block_events {
+                        return ChangeEventRequest::Block;
+                    }
+                }
+
+                ChangeEventRequest::None
+            }
+        }
+    }
+}
+
