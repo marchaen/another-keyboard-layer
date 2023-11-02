a1415497584fe6d0679adfd3508e397e5f7ae97b
marchaen <marchaen.dev@gmail.com>
2023-11-02 10:11:40 +0100

    Add creation of zip of whole build to release script

--- release-helper.sh
+++ release-helper.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 
 out_dir="$(pwd)"/release-build
+version=$(git describe --tags --abbrev=0)
 
 set -x
 
 [ -d "$out_dir" ] && rm -rf "$out_dir"
-
 mkdir "$out_dir"
 
-./clean-output.py
-
-./build-documentation.py
-cp ./documentation-build/README.* "$out_dir"
+echo "$version" > "$out_dir"/VERSION_INFO
+git describe --tags > "$out_dir"/BUILD_INFO
 cp ./LICENSE "$out_dir"
 
-git describe --tags --abbrev=0 > "$out_dir"/VERSION_INFO
-git describe --tags > "$out_dir"/BUILD_INFO
+./clean-output.py
+./build-documentation.py
+cp ./documentation-build/README.* "$out_dir"
 
 cd ./AKL.Cli
 dotnet publish --configuration Release --os win
 cp bin/Release/net7.0/win-x64/publish/* "$out_dir"
+cd -
+
+cd "$out_dir"
+zip -r -9 "another-keyboard-layer-v$version.zip" ./*
