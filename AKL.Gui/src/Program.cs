58d4fb7b729878e8a825dd37c4990c7eeb6c5d27
marchaen <marchaen.dev@gmail.com>
2023-11-03 12:25:49 +0100

    Fix console showing up when building from linux

--- AKL.Gui/src/Program.cs
+++ AKL.Gui/src/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Avalonia;
 using System;
 
+using AKL.Common;
+
 namespace AKL.Gui;
 
 class Program
@@ -10,8 +12,18 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        // The console has to be hidden manually because setting the output type
+        // to winexe in the csproj isn't supported when building from linux. 
+        //
+        // Related issue and question: 
+        // https://github.com/dotnet/sdk/issues/3309
+        // https://stackoverflow.com/questions/64588598/app-opens-console-window-when-being-build-with-docker
+        ConsoleWindowHider.Execute();
+
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
